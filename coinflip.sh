b79229182580a516461909d88b16959b9ada7593
Subhamdas20 <subham21.sd@gmail.com>
2021-08-22 01:55:20 +0530

    Multiple flips

--- coinflip.sh
+++ coinflip.sh
@@ -3,13 +3,18 @@
 
 #tails=0
 #heads=1
-rann=$((RANDOM%2))
 
+for ((i=0;i<80;i++))
+do
+rann=$((RANDOM%2))
 if [[ $rann -eq 0 ]]
 then
-	echo "Tails is the winner"
-
+	# echo "Tails is the winner"
+    j=$(($j+1))
 else
-	echo "Heads is the winner"
-
+	# echo "Heads is the winner"
+    k=$(($k+1))
 fi
+done
+echo "Number of times tails won = " $j
+echo "Number of times heads won = " $k
