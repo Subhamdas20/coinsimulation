24e030648772e99012eb007755bedbcb2a4e4784
Subhamdas20 <subham21.sd@gmail.com>
2021-08-22 02:37:14 +0530

    Result of flippin the coin for multiple times

--- coinflip.sh
+++ coinflip.sh
@@ -1,20 +1,48 @@
-#! /bin/bash
+#! /bin/bash 
 
 
 #tails=0
 #heads=1
-
-for ((i=0;i<80;i++))
+head=0;
+tail=0;
+while [[ $head -lt 21 ]] && [[ $tails -lt 21 ]]
 do
 rann=$((RANDOM%2))
-if [[ $rann -eq 0 ]]
+if [[ $rann -eq 0 ]] && [[ $tails -le 21 ]]
 then
 	# echo "Tails is the winner"
-    j=$(($j+1))
-else
+    tails=$(($tails+1))
+elif [[ $rann -eq 1 ]] && [[ $head -le 21 ]]
+	then
 	# echo "Heads is the winner"
-    k=$(($k+1))
+    head=$(($head+1))
+    
+else
+	head=$head;
+	tails=$tails
 fi
+
 done
-echo "Number of times tails won = " $j
-echo "Number of times heads won = " $k
+
+echo "Number of times head won = "$head
+echo ".................................."
+echo "Number of times tails won =" $tails
+echo "......................................"
+if [[ $tails -ge 21 ]]
+then
+	diff1=$(($tails-$head))
+	echo "Tails won by differnce of -" $diff1
+elif [[ $head -ge 21 ]]
+	then
+	diff2=$(($head-$tails))
+	echo "Heads won by differnce of -" $diff2
+elif [[  $head -eq $tails ]]
+	then
+	echo "Its a tie"
+else
+diff2=$diff2
+fi
+
+# echo "Number of times tails won = " $j
+# echo "Number of times heads won = " $k
+
