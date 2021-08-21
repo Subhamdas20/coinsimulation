b70e7a605e718a237171751357a1cefe469fba29
Subhamdas20 <subham21.sd@gmail.com>
2021-08-22 03:48:54 +0530

    extentions of uc3

--- coinflip.sh
+++ coinflip.sh
@@ -4,17 +4,19 @@
 #tails=0
 #heads=1
 head=0;
-tail=0;
+tails=0;
+# head2=0;
+# tails2=0;
 while [[ $head -lt 21 ]] && [[ $tails -lt 21 ]]
 do
 rann=$((RANDOM%2))
 if [[ $rann -eq 0 ]] && [[ $tails -le 21 ]]
 then
-	# echo "Tails is the winner"
+	
     tails=$(($tails+1))
 elif [[ $rann -eq 1 ]] && [[ $head -le 21 ]]
 	then
-	# echo "Heads is the winner"
+	
     head=$(($head+1))
     
 else
@@ -36,13 +38,31 @@ elif [[ $head -ge 21 ]]
 	then
 	diff2=$(($head-$tails))
 	echo "Heads won by differnce of -" $diff2
-elif [[  $head -eq $tails ]]
-	then
-	echo "Its a tie"
+
 else
-diff2=$diff2
+echo "Its a tie"
 fi
 
-# echo "Number of times tails won = " $j
-# echo "Number of times heads won = " $k
+
+if [[  $head -eq $tails ]]             #when their is a tie
+	then
+	while [[ $(($head-$tails)) -lt 2 ]]  && [[ $(($tails-$head)) -lt 2 ]] 
+		do
+			rann=$((RANDOM%2))
+			if [[ $rann -eq 0 ]] 
+				then
+				echo "Tails is the winner"
+    			tails=$(($tails+1))
+			elif [[ $rann -eq 1 ]]
+				then
+				echo "Heads is the winner"
+    			head=$(($head+1))
+    	
+			else
+				head=$head;
+				tails=$tails
+			fi
+		done
+
+fi
 
